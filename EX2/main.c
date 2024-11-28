eafb5b491e56e313b4c9f9434bb2bb863ba92ae1
Beebee1201 <93655718+Beebee1201@users.noreply.github.com>
2024-11-28 13:41:19 +0800

    Update main.c

--- EX2/main.c
+++ EX2/main.c
@@ -3,13 +3,13 @@
 
 int main()
 {
-    printf("   Norvember 2024");
-    printf("Mo Tu We Th Fr Sa Su");
-    printf("                1  2");
-    printf(" 3  4  5  6  7  8  9");
-    printf("10 11 12 13 14 15 16");
-    printf("17 18 19 20 21 22 23");
-    printf("24 25 26 27 28 29 30");
+    printf("   Norvember 2024\n");
+    printf("Mo Tu We Th Fr Sa Su\n");
+    printf("                1  2\n");
+    printf(" 3  4  5  6  7  8  9\n");
+    printf("10 11 12 13 14 15 16\n");
+    printf("17 18 19 20 21 22 23\n");
+    printf("24 25 26 27 28 29 30\n");
     
 
     return 0;
