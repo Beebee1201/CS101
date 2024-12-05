088746eacb5616048fed42309ed20303a273bc63
Beebee1201 <93655718+Beebee1201@users.noreply.github.com>
2024-12-05 15:31:18 +0800

    Update main2.c

--- EX3/main2.c
+++ EX3/main2.c
@@ -1,1 +1,21 @@
 
+#include <stdio.h>
+
+int main()
+{
+    int i = 2;
+    if (i%15== 0){
+        printf("LOVE IU");
+    }
+    else if (i%5 == 0){
+        printf("IU");
+    }
+    else if (i%3 == 0){
+        printf("LOVE");
+    }
+    else{
+        printf("%d",i);
+    }
+    
+    return 0;
+}
