a9b6a1fa2ff01a6b92664b94a9f046be285f2135
Beebee1201 <93655718+Beebee1201@users.noreply.github.com>
2024-12-05 15:43:42 +0800

    Update main5.c

--- EX3/main5.c
+++ EX3/main5.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main()
+{
+    int i = 99;
+    if (i<=30){
+        printf("免費");
+    }
+    else if (i>240){
+        i = 240;
+    }
+    else if (i%30){
+        i = (i/30)*30+30;
+    }
+    printf("%d 元",i);
+    
+    
+    return 0;
+}
