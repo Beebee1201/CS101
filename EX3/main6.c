4d2723c54412f9670b01bc83c42e7d0dd8f4c274
Beebee1201 <93655718+Beebee1201@users.noreply.github.com>
2024-12-05 15:49:50 +0800

    Update main6.c

--- EX3/main6.c
+++ EX3/main6.c
@@ -1,1 +1,23 @@
 
+#include <stdio.h>
+
+int main()
+{
+    int m,i = 1540;
+    if (i<=1500){
+        m = 70;
+    }
+    else{
+        if (i%100){
+            m = ((i-1500)/100)*10 + 10+70;
+        }
+        else{
+            m = ((i-1500)/100)*10+70;
+        }
+    }
+
+    printf("%d 元",m);
+    
+    
+    return 0;
+}
