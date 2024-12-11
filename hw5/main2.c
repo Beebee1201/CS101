c71b5840fa659172e2f543b7405fff3864420c79
Beebee1201 <93655718+Beebee1201@users.noreply.github.com>
2024-12-11 22:26:46 +0800

    Update main2.c

--- hw5/main2.c
+++ hw5/main2.c
@@ -1,1 +1,25 @@
 
+#include <stdio.h>
+#include <math.h> 
+
+int main() {
+    double pi = 4.0;
+    int count = 0;
+    for(int i = 3;;i+=2){
+ 
+        if(count%2 ==1){
+            pi = pi+(4.0/i);
+        }
+        else{
+            pi = pi-(4.0/i);
+        }
+        count++;
+
+        if(round(pi * 100000) / 100000 == 3.14159){
+            break;
+        }
+    }
+    printf("%d",(count*2)+1);
+    
+    return 0;
+}
