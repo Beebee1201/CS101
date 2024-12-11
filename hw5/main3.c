82f3f189af8448be403e0d069982b417512c8d3a
Beebee1201 <93655718+Beebee1201@users.noreply.github.com>
2024-12-11 22:35:02 +0800

    Create main3.c

---
+++ hw5/main3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    for(int i = 1;i<=9;i++){
+        printf("%d*1=%d\t",i,i*1);
+        printf("%d*2=%d\t",i,i*2);
+        printf("%d*3=%d\t",i,i*3);
+        printf("%d*4=%d\t",i,i*4);
+        printf("%d*5=%d\t",i,i*5);
+        printf("%d*6=%d\t",i,i*6);
+        printf("%d*7=%d\t",i,i*7);
+        printf("%d*8=%d\t",i,i*8);
+        printf("%d*9=%d\n",i,i*9);
+        
+    }
+
+    return 0;
+}
