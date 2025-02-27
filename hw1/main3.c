e140d01aa64c7798d03689b9fc083121b3bcf7c7
Beebee1201 <93655718+Beebee1201@users.noreply.github.com>
2025-02-27 14:59:30 +0800

    Create main3.c

---
+++ hw1/main3.c
@@ -0,0 +1,15 @@
+
+int main()
+{
+    char a[] = "10001111";
+    int ans = 0;
+    for(int i = 0;i<8;i++){
+        if (a[i] == '1'){
+            ans+=pow(2,7-i);
+        }
+    }
+    printf("%X",ans);
+    
+
+    return 0;
+}
